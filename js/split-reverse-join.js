5b1bef84cc3bbe8b9ad522d1d0b4d6869305fdd7
TanaraCavalcante <tanaracavalcante@icloud.com>
2025-01-20 14:05:50 +0100

    write a function to convert a name in initials

--- js/split-reverse-join.js
+++ js/split-reverse-join.js
@@ -7,3 +7,13 @@ function nameShuffler(str){
     //Shuffle It
     return str.split(' ').reverse().join(' ');
   }
+
+
+
+//   Escreva uma função para converter um nome em iniciais. Este kata pega estritamente duas palavras com um espaço entre elas.
+// A saída deve ser duas letras maiúsculas com um ponto separando-as.
+
+// Deve ficar assim:
+// Sam Harris => S.H
+// patrick feeney => P.F
+
