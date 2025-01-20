6ff96a57d37dd4c8cc1425332118ec0101fd1dda
TanaraCavalcante <tanaracavalcante@icloud.com>
2025-01-20 13:50:42 +0100

    create a funtion with a greet in usuary language.

---
+++ js/language-greet.js
@@ -0,0 +1,7 @@
+// O BA da sua startup disse ao marketing que seu site tem um grande público na Escandinávia e países vizinhos. O marketing acha que seria ótimo receber os visitantes no site em seu próprio idioma. Felizmente, você já usa uma API que detecta a localização do usuário, então essa é uma vitória fácil.
+
+// A tarefa
+// Pense em uma maneira de armazenar os idiomas como um banco de dados. Os idiomas estão listados abaixo para que você possa copiar e colar!
+// Escreva uma função 'welcome' que receba um parâmetro 'language', com um tipo String, e retorne uma saudação - se você tiver em seu banco de dados. O padrão deve ser inglês se o idioma não estiver no banco de dados ou no caso de uma entrada inválida.
+// O banco de dados
+// Por favor, modifique isso conforme apropriado para seu idioma.
