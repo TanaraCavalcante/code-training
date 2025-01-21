445504a1fb103140d05b705d41e5cb0d654d6f37
TanaraCavalcante <tanaracavalcante@icloud.com>
2025-01-21 09:51:27 +0100

    create a funtion to array min and max

---
+++ js/arrays.js
@@ -0,0 +1,6 @@
+// História
+// Ben tem uma ideia muito simples para lucrar: ele compra algo e vende novamente. Claro, isso não lhe daria lucro algum se ele simplesmente comprasse e vendesse pelo mesmo preço. Em vez disso, ele vai comprar pelo menor preço possível e vender pelo maior.
+
+// Tarefa
+// Escreva uma função que retorne o número mínimo e máximo da lista/matriz fornecida.
+
