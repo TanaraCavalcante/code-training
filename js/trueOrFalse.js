897769c9a24c1d8653320a17256e746a4b389c4a
TanaraCavalcante <tanaracavalcante@icloud.com>
2025-01-22 11:41:00 +0100

    problem with string

--- js/trueOrFalse.js
+++ js/trueOrFalse.js
@@ -1,3 +1,4 @@
+//! QUESTION
 // Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
 // (In this case, all triangles must have surface greater than 0 to be accepted).
 
@@ -12,3 +13,13 @@ function isTriangle(a,b,c){
     // Verifica se os três lados são positivos e se satisfazem a desigualdade triangular
     return a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a;
  }
+
+
+ //!QUESTION
+//  Complete a solução para que ela retorne true se o primeiro argumento (string) passado terminar com o 2º argumento (também uma string).
+
+//  Exemplos:
+ 
+//  solution('abc', 'bc') // retorna true
+//  solution('abc', 'd') // retorna false
+
