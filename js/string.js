3f58dc0c209d6372f013bdf789b42d0b1e632762
TanaraCavalcante <tanaracavalcante@icloud.com>
2025-01-23 16:27:40 +0100

    solution to getMiddle function

--- js/string.js
+++ js/string.js
@@ -10,3 +10,12 @@
 // "middle" --> "dd"
 // "A" --> "A"
 
+function getMiddle(s) {
+    let length = s.length;
+    
+    if(length % 2 === 0){
+      return s.substring(length / 2 - 1, length / 2 + 1)
+    }else{
+        return s.charAt(Math.floor(length / 2));
+    }
+  }
