1cd5573f0013138db647ac7866c8b793fdedf0f8
TanaraCavalcante <tanaracavalcante@icloud.com>
2025-01-24 14:34:30 +0100

    solution with method if and else

--- js/simpleCalculation.js
+++ js/simpleCalculation.js
@@ -1,3 +1,12 @@
 //! QUESTION
 
 // Este kata consiste em multiplicar um determinado número por oito se for um número par e por nove caso contrário.
+
+function simpleMultiplication(number) {
+    // your code........
+ if(number % 2 === 0){
+   return number * 8;
+ }else{
+   return number * 9;
+ }
+}
