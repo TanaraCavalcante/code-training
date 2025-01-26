e2010b755e68763208acd673212c16c0826e2e0b
TanaraCavalcante <tanaracavalcante@icloud.com>
2025-01-26 17:44:20 +0100

    solution filter_list

--- js/filter.js
+++ js/filter.js
@@ -1,3 +1,7 @@
 //! QUESTION
 // Neste kata, você criará uma função que recebe uma lista de inteiros não negativos e strings e retorna uma nova lista com as strings filtradas.
 
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+     return l.filter(item => typeof item === "number");
+  }
